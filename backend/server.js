64f248eff1f0908cc24a667fce63a5f04be8a96e
Saloni-01 <Saloni198400@gmail.com>
2026-03-14 12:08:22 +0530

    Fix login route

--- backend/server.js
+++ backend/server.js
@@ -89,7 +89,7 @@ const transporter = nodemailer.createTransport({
 LOGIN API
 ====================== */
 
-app.post("/api/login", (req, res) => {
+app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
